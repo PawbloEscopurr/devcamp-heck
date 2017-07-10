41c325a9e3223fa706ffb827b58a76b715b82336
Jordan Hudgens <jordan@devcamp.com>
2017-07-10 15:20:43 +0000

    Implemented concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,18 +1,18 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
   
   #Pretty much just references on how to create a custom scope. See 'How to Implement Custom Scopes in Rails 5' in section 2 for more instruction.
   def self.angular
     where(subtitle: 'Angular')
   end
+
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
-  
-  after_initialize :set_defaults 
 
+  after_initialize :set_defaults
 
-    def set_defaults
-    self.main_image ||= "http://placehold.jp/600x400.png"
-    self.thumb_image||= "http://placehold.jp/350x200.png"
-    end
+  def set_defaults
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+  end
 end
-
